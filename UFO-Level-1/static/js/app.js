f2d276c538a10ae2c9f9ab250fd935bfbaf712a1
Vince <Vince@MacBook-Air.local>
2019-10-25 20:37:14 -0500

    vb

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -2,3 +2,37 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+button.on("click", function() {
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+  console.log(tableData);
+
+  var filteredData = tableData.filter(ufo => ufo.datetime === inputValue);
+
+  console.log(filteredData);
+
+  var tbody = d3.select("tbody");
+
+  filteredData.forEach(function(result) {
+    console.log(result);
+    var row = tbody.append("tr");
+    Object.entries(result).forEach(function([key, value]) {
+      console.log(key, value);
+      // Append a cell to the row for each value
+      // in the weather report object
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+});
